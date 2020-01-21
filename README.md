1d8e2a796239c73dd4f0ff090611169751a6cbf3
tnagels <thomas@Nieuwe-Grijze-6.thuis>
2020-01-21 23:07:16 +0100

    FInal edits

--- README.md
+++ README.md
@@ -3,21 +3,35 @@ _Based on a Gist by Claudio Bartoli_
 
 A lot of work still needs to be done as the code is not very nice, still working on it.
 
+You have to define the lights one by one, not in a group as this does not allow reading of the values.
 ```yaml
 ---
-hello_world:
-  module: hello
-  class: HelloWorld
-remote_bureau_brightness:
-  module: tradfri
-  class: TradfriRemoteDimmer
-  lights:
-    - light.pendel_bureau
-  remote:
-    id: tradfri_remote_control
-    type: ROUND
-  dimmer:
-    step: 10
-    min: 0
-    max: 255
+  remote_aanrecht_brightness:
+    module: tradfri
+    class: TradfriRemoteDimmer
+    lights:
+      - light.inbouwspots_aanrecht_keuken
+      - light.ledstrip_aanrecht_keuken
+    remote:
+      id: tradfri_remote_control
+      type: ROUND
+    dimmer:
+      step: 10
+      min: 0
+      max: 255
+
+  remote_bureau_brightness:
+    module: tradfri
+    class: TradfriRemoteDimmer
+    lights:
+      - light.pendel_bureau
+      - light.inbouwspots_bureau
+      - light.opbouwspots_bureau
+    remote:
+      id: tradfri_on_off_switch
+      type: SQUARE
+    dimmer:
+      step: 10
+      min: 0
+      max: 255
 ```
--- apps/tradfri/tradfri.py
+++ apps/tradfri/tradfri.py
@@ -9,7 +9,7 @@ class TradfriRemoteDimmer(hass.Hass):
     self.dim_max = self.args['dimmer']['max']
     self.dim_step = self.args['dimmer']['step']
     self.dim_action = 0
-    self.target_remote_type = self.args['remote']['id']
+    self.target_remote_type = self.args['remote']['type']
     self.log(f"Initialized dimming to IKEA tradfri remote control [{self.target_remote}] type [{self.target_remote_type}] on target lights [{self.target_lights}]")
     self.listen_event(self.deconz_event, "deconz_event", id = self.target_remote)
     if self.target_remote_type == 'ROUND': self.remote_events = {'UP_HOLD': 2001,'UP_CLICK': 2002,'DOWN_HOLD': 3001,'DOWN_CLICK': 3002}
@@ -21,10 +21,10 @@ class TradfriRemoteDimmer(hass.Hass):
       value = self.get_state(light, attribute = "brightness")
       if value is None: value = 0
       value += self.dim_action
-      if self.dim_min >= value:
+      if self.dim_min > value:
         self.dim_action = 0
         value = self.dim_min
-      elif value >= self.dim_max:
+      elif value > self.dim_max:
         self.dim_action = 0
         value = self.dim_max
       for light in self.target_lights:
@@ -43,6 +43,7 @@ class TradfriRemoteDimmer(hass.Hass):
       self.dim_action = 0
       for light in self.target_lights:
         self.turn_on(light)
+      self.log('OK')
     elif data['event'] == self.remote_events['DOWN_CLICK']:
       self.dim_action = 0
       for light in self.target_lights:
