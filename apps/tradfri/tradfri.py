c72555c898bbb869e10b9dd1c97a5e77a2b44320
tnagels <thomas@Nieuwe-Grijze-6.local>
2020-01-21 20:56:11 +0100

    Single file

--- apps/tradfri/tradfri.py
+++ apps/tradfri/tradfri.py
@@ -1,8 +1,5 @@
 import appdaemon.plugins.hass.hassapi as hass
 
-from dimmer import Dimmer
-from tradfri_remote import TradfriRemoteButtons
-
 class TradfriRemoteDimmer(hass.Hass):
 
   def initialize(self):
@@ -10,31 +7,23 @@ class TradfriRemoteDimmer(hass.Hass):
     self.target_light = self.args['target']['light']
     self.target_remote = self.args['target']['remote']
     self.dim_attribute = self.args['dimmer']['attribute']
-
-    self.button = TradfriRemoteButtons()
-
-    self.dimmer = Dimmer(
-      self.args['dimmer']['down']['limit'],
-      self.args['dimmer']['up']['limit'],
-      self.args['dimmer']['step'],
-      self.get,
-      self.set
-    )
+    self.dim_min = self.args['dimmer']['down']['limit']
+    self.dim_max = self.args['dimmer']['up']['limit']
+    self.dim_step = self.args['dimmer']['step']
 
     self.log(f"Initialized [{self.dim_attribute}] dimming to IKEA tradfri remote control [{self.target_remote}] on target lights [{self.target_light}]")
 
     self.listen_event(self.deconz_event, "deconz_event", id = self.target_remote)
 
   def deconz_event(self, event_name, data, kwargs):
-
-    self.button.long_press_handle(data['event'], self.args['dimmer']['up']['button'], self.dimmer.up, self.dimmer.stop)
-    self.button.long_press_handle(data['event'], self.args['dimmer']['down']['button'], self.dimmer.down, self.dimmer.stop)
-
-  def get(self):
-    value = self.get_state(self.target_light, attribute = self.dim_attribute)
-    if value is None:
-      value = 0
-    return value
-
-  def set(self, value):
-    self.turn_on(self.target_light, **{self.dim_attribute: value})
+    if data['event'] == 2001:
+      self.log("UP")
+      step =
+    elif data['event'] == 3001:
+      self.log("DOWN")
+    else:
+      self.log("STOP")
+#    value = self.get_state(self.target_light, attribute = self.dim_attribute)
+#    if value is None:
+#      value = 0
+#    self.turn_on(self.target_light, **{self.dim_attribute: value})
