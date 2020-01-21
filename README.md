5a98f8e9263a7a9632ed456fa9a056262ef70da6
tnagels <thomas@Nieuwe-Grijze-6.local>
2020-01-21 22:51:01 +0100

    Allow for multiple lights at once

--- README.md
+++ README.md
@@ -5,15 +5,19 @@ A lot of work still needs to be done as the code is not very nice, still working
 
 ```yaml
 ---
-remote_corridoio_brightness:
+hello_world:
+  module: hello
+  class: HelloWorld
+remote_bureau_brightness:
   module: tradfri
   class: TradfriRemoteDimmer
-  target:
-    light: light.office
-    remote: tradfri_remote_control
-    type: SQUARE
+  lights:
+    - light.pendel_bureau
+  remote:
+    id: tradfri_remote_control
+    type: ROUND
   dimmer:
-    step: 15
+    step: 10
     min: 0
     max: 255
 ```
--- apps/tradfri/tradfri.py
+++ apps/tradfri/tradfri.py
@@ -3,46 +3,49 @@ import appdaemon.plugins.hass.hassapi as hass
 class TradfriRemoteDimmer(hass.Hass):
 
   def initialize(self):
-    self.target_light = self.args['target']['light']
-    self.target_remote = self.args['target']['remote']
+    self.target_lights = self.args['lights']
+    self.target_remote = self.args['remote']['id']
     self.dim_min = self.args['dimmer']['min']
     self.dim_max = self.args['dimmer']['max']
     self.dim_step = self.args['dimmer']['step']
     self.dim_action = 0
-    self.target_remote_type = self.args['target']['type']
+    self.target_remote_type = self.args['remote']['id']
     self.log(f"Initialized dimming to IKEA tradfri remote control [{self.target_remote}] type [{self.target_remote_type}] on target lights [{self.target_light}]")
     self.listen_event(self.deconz_event, "deconz_event", id = self.target_remote)
     if self.target_remote_type == 'ROUND': self.remote_events = {'UP_HOLD': 2001,'UP_CLICK': 2002,'DOWN_HOLD': 3001,'DOWN_CLICK': 3002}
     else: self.remote_events = {'UP_HOLD': 1001,'UP_CLICK': 1002,'DOWN_HOLD': 2001,'DOWN_CLICK': 2002}
 
   def dimmer(self, *kwargs):
-    # self.log(self.dim_action)
     if self.dim_action == 0: return
-    value = self.get_state(self.target_light, attribute = self.dim_attribute)
-    if value is None: value = 0
-    value += self.dim_action
-    if self.dim_min >= value:
-      self.dim_action = 0
-      value = self.dim_min
-    elif value >= self.dim_max:
-      self.dim_action = 0
-      value = self.dim_max
-    self.turn_on(self.target_light, **{"brightness": value})
+    for light in self.target_lights:
+      value = self.get_state(light, attribute = "brightness")
+      if value is None: value = 0
+      value += self.dim_action
+      if self.dim_min >= value:
+        self.dim_action = 0
+        value = self.dim_min
+      elif value >= self.dim_max:
+        self.dim_action = 0
+        value = self.dim_max
+      for light in self.target_lights:
+        self.turn_on(light, **{"brightness": value})
     self.run_in(self.dimmer, .5)
 
 
   def deconz_event(self, event_name, data, kwargs):
-    if data['event'] == self_remote_events['UP_HOLD']:
+    if data['event'] == self.remote_events['UP_HOLD']:
       self.dim_action = self.dim_step
       self.dimmer()
-  elif data['event'] == self_remote_events['DOWN_HOLD']:
+    elif data['event'] == self.remote_events['DOWN_HOLD']:
       self.dim_action = -self.dim_step
       self.dimmer()
-  elif data['event'] == self_remote_events['UP_CLICK']:
+    elif data['event'] == self.remote_events['UP_CLICK']:
       self.dim_action = 0
-      self.turn_on(self.target_light)
-  elif data['event'] == self_remote_events['DOWN_CLICK']:
+      for light in self.target_lights:
+        self.turn_on(light)
+    elif data['event'] == self.remote_events['DOWN_CLICK']:
       self.dim_action = 0
-      self.turn_off(self.target_light)
+      for light in self.target_lights:
+        self.turn_off(light)
     else:
       self.dim_action = 0
